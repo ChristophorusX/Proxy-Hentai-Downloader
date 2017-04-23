3799444cd1b9d1d04f75be88329bd33f3487453d
ChristophorusX <mrxrtxrt@gmail.com>
2017-04-23 12:20:35 -0400

    Syn

--- parse_proxies.py
+++ parse_proxies.py
@@ -4,10 +4,14 @@ def parseProxies():
     with open('proxies.txt') as f:
         for line in f:
             line = re.sub(r'^.+China.+$', '', line)
+            print line
 
     with open('proxies.txt') as f:
         for line in f:
             line = re.sub(r'^(.+)#.+$',r'\1', line)
+            print line
+
+
 
     with open('proxies.txt', 'r') as f:
         proxyList = [line[:-1] for line in f]
