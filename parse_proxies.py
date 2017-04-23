96d34a0d6203bab899a316988190d760128cc83a
ChristophorusX <mrxrtxrt@gmail.com>
2017-04-23 12:51:37 -0400

    Syn

--- parse_proxies.py
+++ parse_proxies.py
@@ -1,16 +1,13 @@
 import re
 
 def parseProxies():
-    with open('proxies.txt') as f:
-        for line in f:
-            line = re.sub(r'^.+China.+$', '', line)
-
-    with open('proxies.txt') as f:
-        for line in f:
-            line = re.sub(r'^(.+)#.+$',r'\1', line)
-            print(line)
-
-
+    with open('fetched_proxies.txt') as old_file:
+        with open('proxies.txt', 'w') as new_file:
+            for line in old_file:
+                line = re.sub(r'^.+China.+$', '', line)
+                line = re.sub(r'^(.+)#.+$',r'\1', line)
+                if line != '':
+                    new_file.write(line)
 
     with open('proxies.txt', 'r') as f:
         proxyList = [line[:-1] for line in f]
