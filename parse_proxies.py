de861c0bae33e0fd9070d7df392bcd7c9097683d
ChristophorusX <mrxrtxrt@gmail.com>
2017-04-23 16:09:56 -0400

    Syn

--- parse_proxies.py
+++ parse_proxies.py
@@ -5,9 +5,11 @@ def parseProxies():
         with open('proxies.txt', 'w') as new_file:
             pattern_with_china = re.compile('^.+china.+$')
             patter_with_pound = re.compile('^(\S+)(\s+)#.+$')
+            patter_before = re.compile('.+(http|socks)(.+)')
             for line in old_file:
                 line = re.sub(pattern_with_china, '', line)
                 line = re.sub(patter_with_pound, r'\1', line)
+                line = re.sub(patter_before, r'\1\2', line)
                 if re.match(r'.+', line):
                     new_file.write(line)
 
