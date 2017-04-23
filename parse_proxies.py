94666a881538f773e7055f3dc6474aae002d96fe
ChristophorusX <mrxrtxrt@gmail.com>
2017-04-23 13:02:35 -0400

    Syn

--- parse_proxies.py
+++ parse_proxies.py
@@ -3,10 +3,12 @@ import re
 def parseProxies():
     with open('fetched_proxies.txt') as old_file:
         with open('proxies.txt', 'w') as new_file:
+            pattern_with_china = re.compile('^.+china.+$')
+            patter_with_pound = re.compile('^(.+)#.+$',r'\1')
             for line in old_file:
-                line = re.sub(r'^.+China.+$', '', line)
-                line = re.sub(r'^(.+)#.+$',r'\1', line)
-                if line != '':
+                line = re.sub(pattern_with_china, '', line)
+                line = re.sub(patter_with_pound, r'\1', line)
+                if re.match(r'.+', line):
                     new_file.write(line)
 
     with open('proxies.txt', 'r') as f:
