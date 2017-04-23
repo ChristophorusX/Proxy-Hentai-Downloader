59d2086dfdeea8d295a7eb0dbf0c9851da144e59
ChristohoprusX <mrxrtxrt@gmail.com>
2017-04-23 10:55:00 -0400

    Transfer

--- parse_proxies.py
+++ parse_proxies.py
@@ -1,4 +1,10 @@
+import re
+
 def parseProxies():
+    with open('proxies.txt') as f:
+        for line in f:
+            re.sub(r'*China*','',s)
+
     with open('proxies.txt', 'r') as f:
         proxyList = [line[:-1] for line in f]
         return proxyList
