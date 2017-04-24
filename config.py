44098b57b3a940dde421a19edfe08a34d9319a66
ChristohoprusX <mrxrtxrt@gmail.com>
2017-04-23 20:31:02 -0400

    syn

---
+++ config.py
@@ -0,0 +1,16 @@
+import parse_proxies
+
+dir = "/home/christophorus/Desktop/Downloaded Manga"
+download_ori = False
+download_thread_cnt = 5
+scan_thread_cnt = 1
+proxy = parse_proxies.parseProxies()
+log_path = "eh.log"
+log_verbose = 2
+rename_ori = False
+daemon = False
+rpc_interface = 'localhost'
+rpc_port = None
+rpc_secret = None
+save_tasks = False
+make_archive = False
