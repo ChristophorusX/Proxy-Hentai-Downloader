e1046f15de214135e56371345d8c994674b63099
ChristophorusX <mrxrtxrt@gmail.com>
2017-04-23 21:20:58 -0400

    Syn

--- parse_proxies.py
+++ parse_proxies.py
@@ -1,23 +1,22 @@
 import re
 
 def parseProxies():
-    with open('fetched_proxies.txt') as old_file:
-        with open('proxies.txt', 'w') as new_file:
-            pattern_with_china = re.compile('^.+china.+$')
-            patter_with_pound = re.compile('^(\S+)(\s+)#.+$')
-            patter_before = re.compile('.+(http|socks)(.+)')
-            for line in old_file:
-                if not patter_before.match(line):
-                    continue
-                line = re.sub(patter_before, r'\1\2', line)
-                line = re.sub(pattern_with_china, '', line)
-                line = re.sub(patter_with_pound, r'\1', line)
-                if re.match(r'.+', line):
-                    new_file.write(line)
+    # with open('fetched_proxies.txt') as old_file:
+    #     with open('proxies.txt', 'w') as new_file:
+    #         pattern_with_china = re.compile('^.+china.+$')
+    #         patter_with_pound = re.compile('^(\S+)(\s+)#.+$')
+    #         patter_before = re.compile('.+(http|socks)(.+)')
+    #         for line in old_file:
+    #             if not patter_before.match(line):
+    #                 continue
+    #             line = re.sub(patter_before, r'\1\2', line)
+    #             line = re.sub(pattern_with_china, '', line)
+    #             line = re.sub(patter_with_pound, r'\1', line)
+    #             if re.match(r'.+', line):
+    #                 new_file.write(line)
 
-    with open('proxies.txt', 'r') as f:
-        for line in f:
-            print(line)
+    # with open('proxies.txt', 'r') as f:
+      with open('fetched_proxies.txt', 'r') as f:
         proxyList = [line[:-1] for line in f]
         return proxyList
 
