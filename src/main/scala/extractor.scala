653c3ba11c0aa94c6375c918d9384dd5aae644c1
ChristophorusX <mrxrtxrt@gmail.com>
2017-10-10 12:06:40 -0400

    neat update

--- src/main/scala/extractor.scala
+++ src/main/scala/extractor.scala
@@ -6,30 +6,22 @@ import java.io.PrintWriter
 object Extractor {
   def main(args: Array[String]): Unit = {
     val urlFile = new File("/Users/christophorus/Desktop/Proxy-Hentai-Downloader/urls.txt")
-    val urlBufferWriter = new PrintWriter(urlFile)
-    // for (i <- 0 to 16) {
-    //   val documentName = "view-source_https___exhentai.org_favorites.php_page=" + i + ".html"
-    //   println("Now parsing document " + documentName)
-    //   val resultUrlList = parse(documentName)
-    //   urlBufferWriter.write(" " + resultUrlList)
-    //   println("Document " + documentName + " has been parsed!")
-    // }
+    val urlPrintWriter = new PrintWriter(urlFile)
     val documentName = "view-source_https___exhentai.org_favorites.php.html"
-    println("Now parsing document " + documentName)
-    val resultUrlList = parse(documentName)
-    urlBufferWriter.write(" " + resultUrlList)
-    println("Document " + documentName + " has been parsed!")
-    urlBufferWriter.close()
+    println(s"Now parsing document $documentName")
+    val resultUrlString = parse(documentName)
+    urlPrintWriter.write(" " + resultUrlString)
+    println(s"Document $documentName has been parsed!")
+    urlPrintWriter.close()
   }
 
-  def parse(documentName: String): String = {
+  val parse = (documentName: String) => {
     val pattern = new Regex("https://exhentai.org/g/[0-9]+/[0-9a-z]{10}/")
     val bufferSource = Source.fromFile("/Users/christophorus/Downloads/php/" + documentName)
-    var returnString = ""
-    for (line <- bufferSource.getLines()) {
-        val iter = pattern findAllIn line
-        returnString += iter filter (x => x equals iter.next) mkString (" ")
-    }
-    returnString
+    val fileString = bufferSource.getLines().mkString
+    bufferSource.close
+    val urlList = pattern.findAllIn(fileString).toList
+    val cleanList = for {(url, count) <- urlList.zipWithIndex if count % 2 == 0} yield url
+    cleanList.mkString(" ")
   }
 }
